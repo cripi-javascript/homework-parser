02d9ea372618851640633fa990f517dd2b7d5089
Mikhail Davydov <azazel.private@gmail.com>
2012-11-07 22:36:02 +0600

    total table field, better promise usage

--- parser.js
+++ parser.js
@@ -2,7 +2,7 @@
 
 function getHomeWorks(host) {
     var dfd = $.Deferred();
-    
+
     $.get(host).done(function (html) {
         var homeworks = $(html).find('.js-repo-list').find('h3 a')
         .map(function () {
@@ -13,16 +13,16 @@ function getHomeWorks(host) {
             return (/^dz-/).test(item);
         })
         .sort();
-        
+
         dfd.resolve(homeworks);
     });
-    
+
     return dfd.promise();
 }
 
 function getPullRequests(repo) {
     var dfd = $.Deferred();
-    
+
     $.get('/cripi-javascript/' + repo + '/pulls')
     .done(function (html) {
         var pulls = $(html).find('.pulls-list').find('.gravatar')
@@ -30,13 +30,13 @@ function getPullRequests(repo) {
             return $(this).attr('href').replace('/', '');
         })
         .get();
-        
+
         dfd.resolve({
             repo: repo,
             pulls: pulls
         });
     });
-    
+
     return dfd.promise();
 }
 
@@ -44,31 +44,41 @@ function printResultsTable(results) {
     var headers = ['name'],
         table = [],
         index = {};
-    
+
     results.forEach(function (task) {
         headers.push(task.repo);
-        
+
         task.pulls.forEach(function (student) {
             if (!index[student]) {
                 index[student] = {};
                 index[student].name = student;
+                index[student].total = 0;
                 table.push(index[student]);
             }
             index[student][task.repo] = 1;
+            index[student].total++;
         });
     });
-    
-    console.table(table, headers);
+
+    headers.push('total');
+
+    if (console.table) {
+        console.table(table, headers);
+    } else {
+        console.log(table);
+    }
 }
 
-getHomeWorks('/cripi-javascript').then(function (homeworks) {
+getHomeWorks('/cripi-javascript')
+.then(function (homeworks) {
     var promises = homeworks.map(function (homework) {
         return getPullRequests(homework);
     });
-    
-    $.when.apply($, promises).then(function () {
-        printResultsTable([].slice.call(arguments));
-    });
+
+    return $.when.apply($, promises);
+})
+.then(function () {
+    printResultsTable([].slice.call(arguments));
 });
 
 }());
